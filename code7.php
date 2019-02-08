23d42f20d9f91303143ccf66f9a981d477d4255e
Enrico Zimuel <e.zimuel@gmail.com>
2019-02-08 14:19:01 +0100

    Rename code files

--- code7.php
+++ code7.php
@@ -11,7 +11,7 @@ $password = 'password';
 // Argon2i without Sodium
 $hash = password_hash($password, PASSWORD_ARGON2I); // 95 bytes
 // Argon2id with PHP 7.3
-$hash2 = password_hash($password, PASSWORD_ARGON2ID);
+$hash2 = password_hash($password, PASSWORD_ARGON2ID // 96 bytes
 
 echo password_verify($password, $hash) ? 'OK' : 'Error';
 echo password_verify($password, $hash2) ? 'OK' : 'Error';
